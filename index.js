6d8b4fcea04f5d6bef035ab2ae0f72f7c7cd0c2d
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-22 16:39:50 -0400

    data attributes can be underscore prefixed

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ angular.module("ngPush", []).directive("ngPush", function() {
       var keys = Object.keys(attrs);
       angular.forEach(keys, function(k) {
         if (!~ignore.indexOf(k)) {
-          obj[k] = attrs[k];
+          // prefix the raw data-___ attribute and strip off `data-` and use
+          // that is the key name
+          var key = attrs.$attr[k].replace(/^data-/, "");
+          obj[key] = attrs[k];
         }
       });
 
--- test/index.js
+++ test/index.js
@@ -30,4 +30,28 @@ describe("ngPush", function() {
       }
     ], scope.files);
   }));
+
+  it("can push underscore prefixed attributes", inject(function($rootScope, $compile) {
+    var scope = $rootScope.$new();
+    scope.files = [];
+    var html = "" +
+      "<ng-push ng-model=\"files\" data-_id=\"1234567890\" data-_name=\"foo\"></ng-push>" +
+      "<ng-push ng-model=\"files\" data-_id=\"0987654321\" data-_name=\"bar\"></ng-push>" +
+      "<ng-push ng-model=\"files\" data-__method=\"DELETE\"></ng-push>";
+    var ele = $compile(html)(scope);
+    assert.lengthOf(scope.files, 3);
+    assert.deepEqual([
+      {
+        _id:   "1234567890",
+        _name: "foo"
+      },
+      {
+        _id:   "0987654321",
+        _name: "bar"
+      },
+      {
+        __method: "DELETE"
+      }
+    ], scope.files);
+  }));
 });
